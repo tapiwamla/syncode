5034227717511d880f1e87a8054c1ff03117dc21
tapiwamla <tmlambo@usiu.ac.ke>
2023-09-19 21:49:00 +0300

    Upload.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Sync Code: Realtime Collaborative Code Editor
+
+## Introduction
+Are you tired of sending code snippets back and forth, struggling to debug and collaborate with your team? Look no further! **Sync Code** is here to revolutionize the way you code together. This powerful and intuitive collaborative code editor is designed to empower developers, designers, and teams to work seamlessly in real-time, regardless of their location. With **Sync Code**, you can code together, debug together, and ship faster, together.
+
+## Features
+- Multiple users can join a room and edit code together
+- Changes are reflected in real time
+- Copy button to copy the room id to clipboard
+- Leave button to leave the room
+- Supports syntax highlighting (currently only for JavaScript)
+- Users can leave the room and rejoin later to continue editing
+- Joining & leaving of users is also reflected in real time
+
+## Tech Stack
+- React.js
+- Node.js
+- Express.js
+- Socket.io
+- CodeMirror
+- React-Toastify
+
+## Run Locally
+This project is not currently live on any server. But to use the demo version, follow the steps below:
+- Clone this repository and cd into it
+- Run `npm install` to install the dependencies
+- To start the react app client run `yarn start` in one terminal
+- To start the server run `yarn server:dev` in another terminal
+- Go to `http://localhost:3000` to view the app
+- To join as an another user open another browser or incognito tab and go to `http://localhost:3000`
+- Enter the same room id to join the same room
+
+Now both your editor will be synced and you can see the changes in real time. Try opening the same room in multiple tabs and see the changes.
+
+## Project Video
+https://github.com/Mohitur669/Realtime-Collaborative-Code-Editor/assets/79283402/14c17cc7-d23a-4d39-8af3-ef9e9fa8ef45
+
+**Note:** If you find any bugs, please let me know. I will try to fix it as soon as possible :) <br>
+In case you want to fix it yourself, feel free to make a pull request.
+
+## Future Scope 
+- Add syntax highlighting for multiple languages
+- Add support for multiple themes (currently using Dracula theme)
+
+## About Me
+I am Mohd Mohitur Rahaman, a tech geek, currently pursuing a Master's in Computer Applications from KIIT, Bhubaneswar. And with a deep passion for coding and a strong love for science & technology, I am dedicated to honing my skills and achieving proficiency as a developer.
+
+## Connect with me
+- [LinkedIn](https://www.linkedin.com/in/mohitur02/)
---
+++ src/pages/EditorPage.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Client from '../components/Client';
+import Editor from '../components/Editor';
+import { initSocket } from '../socket';
+import ACTIONS from '../actions/Actions';
+import { useLocation, useNavigate, Navigate, useParams } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+function EditorPage() {
+
+    const socketRef = useRef(null);
+    const codeRef = useRef(null);
+    const location = useLocation();
+    const { roomId } = useParams();
+    const reactNavigator = useNavigate();
+    const [ clients, setClients ] = useState([]);
+
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initSocket();
+            socketRef.current.on('connect_error', (err) => handleErrors(err));
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+            function handleErrors(e) {
+                console.log('socket error', e);
+                toast.error('Socket connection failed, try again later.');
+                reactNavigator('/');
+            }
+
+            socketRef.current.emit(ACTIONS.JOIN, {
+                roomId,
+                username: location.state?.username,
+            });
+
+            // Listening for joined event
+            socketRef.current.on(
+                ACTIONS.JOINED,
+                ({ clients, username, socketId }) => {
+                    if (username !== location.state?.username) {
+                        toast.success(`${username} joined the room.`);
+                        console.log(`${username} joined`);
+                    }
+
+                    // Update the clients list with a unique list of clients using socketId
+                    const uniqueClients = clients.filter(
+                        (client, index, self) =>
+                            index === self.findIndex(c => c.username === client.username)
+                    );
+
+                    setClients(uniqueClients);
+                    // for syncing the code from the start
+                    socketRef.current.emit(ACTIONS.SYNC_CODE, {
+                        code: codeRef.current,
+                        socketId,
+                    });
+                });
+
+            // listening for disconnected
+            socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+                if (username) {
+                    toast.success(`${username} left the room`);
+                    setClients((prevClients) => {
+                        return prevClients.filter(client => client.socketId !== socketId);
+                    });
+                }
+            });
+        };
+
+        init();
+
+        // listener cleaning function
+        return () => {
+            if (socketRef.current) {
+                socketRef.current.disconnect();
+                socketRef.current.off(ACTIONS.JOINED);
+                socketRef.current.off(ACTIONS.DISCONNECTED);
+            }
+        }
+    }, [ location.state?.username, reactNavigator, roomId ]);
+
+    // copy the room id to clipboard
+    async function copyRoomId() {
+        try {
+            await navigator.clipboard.writeText(roomId);
+            toast.success('Room ID copied to clipboard');
+        } catch (error) {
+            toast.error('Failed to copy room ID');
+        }
+    }
+
+    // leave the room
+    function leaveRoom() {
+        socketRef.current.emit(ACTIONS.LEAVE_ROOM, {
+            roomId,
+            username: location.state.username,
+        });
+        reactNavigator('/');
+        setTimeout(() => {
+            window.location.reload();
+        }, 100);
+    }
+
+    if (!location.state) {
+        return <Navigate to="/" />
+    }
+
+    return (
+        <div className="mainWrap">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img className="logoImage" src="/logo.png" alt="logo" />
+                    </div>
+
+                    <h3>Connected</h3>
+
+                    <div className="clientsList">
+                        {
+                            clients.map((client) => (
+                                <Client key={client.socketId} username={client.username} />
+                            ))
+                        }
+                    </div>
+                </div>
+
+                <button className="btn copyBtn" onClick={copyRoomId} >Copy ROOM ID</button>
+                <button className="btn leaveBtn" onClick={leaveRoom} >Leave</button>
+            </div>
+
+            <div className="editorWrap">
+                <Editor
+                    socketRef={socketRef}
+                    roomId={roomId}
+                    onCodeChange={(code) => { codeRef.current = code }}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default EditorPage;
